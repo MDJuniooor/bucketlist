a9daa19683a85a4ec3584bd18e55ad1b3e8864ca
DaeSung <anseotjd12@gmail.com>
2018-03-29 17:08:36 +0900

    update

--- bucketlist/frontend/src/components/Bucket/index.js
+++ bucketlist/frontend/src/components/Bucket/index.js
@@ -5,7 +5,7 @@ import { actionCreators as bucketActions } from '../../redux/modules/bucket';
 const mapDispatchToProps = (dispatch, ownProps) => {
     return {
         handleHeartClick: () => {
-            if (ownProps.isLiked) {
+            if (ownProps.is_liked) {
                 dispatch(bucketActions.unlikeBucket(ownProps.id));
             } else {
                 dispatch(bucketActions.likeBucket(ownProps.id));
