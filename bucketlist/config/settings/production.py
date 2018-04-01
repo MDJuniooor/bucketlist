fb3f6539797cf7483775ff84087e57c767242e5c
DaeSung <anseotjd12@gmail.com>
2018-04-01 13:48:44 +0900

    updated allowed_hosts

--- bucketlist/config/settings/production.py
+++ bucketlist/config/settings/production.py
@@ -8,7 +8,7 @@ from .base import env
 # https://docs.djangoproject.com/en/dev/ref/settings/#secret-key
 SECRET_KEY = env('DJANGO_SECRET_KEY')
 # https://docs.djangoproject.com/en/dev/ref/settings/#allowed-hosts
-ALLOWED_HOSTS = env.list('DJANGO_ALLOWED_HOSTS', default=['example.com'])
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost', '52.79.136.178', '13.125.143.20']
 
 # DATABASES
 # ------------------------------------------------------------------------------
